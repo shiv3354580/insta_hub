3543cbe513873339afefbd7a0da05f2f1048777b
shiv3354580 <androess31@gmail.com>
2023-12-10 11:30:31 +0530

    nginx fix

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,7 +4,7 @@
 sudo systemctl daemon-reload
 sudo rm -f /etc/nginx/sites-enabled/default
 
-sudo cp /home/ubuntu/insta_hub/nginx/nginx.conf /etc/nginx/sites-available/
+sudo cp /home/ubuntu/insta_hub/nginx/nginx.conf /etc/nginx/sites-available/insta_hub
 sudo ln -s /etc/nginx/sites-available/insta_hub /etc/nginx/sites-enabled/
 #sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
 #sudo nginx -t
